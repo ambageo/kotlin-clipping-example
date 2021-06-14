55b5dbfd6b4532a1e0ce867886b1649dcca60c2c
GeorgeAmpartzidis <george.ampartzidis@mpway.com>
2021-06-14 09:42:14 +0100

    Initial commit

---
+++ app/src/main/java/com/example/clippingexample/ClippedView.kt
@@ -0,0 +1,12 @@
+package com.example.clippingexample
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class ClippedView @JvmOverloads constructor(
+    context: Context,
+    attributes: AttributeSet? = null,
+    defStyleAttr: Int = 0): View(context, attributes, defStyleAttr){
+
+}
