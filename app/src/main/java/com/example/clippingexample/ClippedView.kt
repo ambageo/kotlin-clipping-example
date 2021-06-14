5815df2741a866a515b78bea4bd20f98b157c2c5
GeorgeAmpartzidis <george.ampartzidis@mpway.com>
2021-06-14 09:50:38 +0100

    Add paint and path objects

--- app/src/main/java/com/example/clippingexample/ClippedView.kt
+++ app/src/main/java/com/example/clippingexample/ClippedView.kt
@@ -1,6 +1,8 @@
 package com.example.clippingexample
 
 import android.content.Context
+import android.graphics.Paint
+import android.graphics.Path
 import android.util.AttributeSet
 import android.view.View
 
@@ -9,4 +11,12 @@ class ClippedView @JvmOverloads constructor(
     attributes: AttributeSet? = null,
     defStyleAttr: Int = 0): View(context, attributes, defStyleAttr){
 
+        private val paint = Paint().apply {
+            // Smooth out edges of what is drawn
+            isAntiAlias = true
+            strokeWidth = resources.getDimension(R.dimen.strokeWidth)
+            textSize = resources.getDimension(R.dimen.textSize)
+        }
+
+    private val path = Path()
 }
